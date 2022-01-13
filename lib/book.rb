47f42e94394430acc5e6793ba80fb0884648c816
maximilianjones <maxcharles500@gmail.com>
2022-01-12 16:13:01 -0800

    commit tree 075d8dc31a263b8cbf97f01d22e812dd9be7ea6f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
+class Book 
+    attr_accessor :title, :page_count, :author, :genre
+    
+    def initialize(title)
+        @title = title
+        @page_count = page_count
+        @author = author
+        @genre = genre
+    end
+   
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
